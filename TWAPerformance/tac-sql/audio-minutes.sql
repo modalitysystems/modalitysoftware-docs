584714b16ee23f85bfbbb5d1cfad9dd774fd27d6
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-02-10 12:49:49 +0000

    Create audio-minutes.sql

---
+++ TWAPerformance/tac-sql/audio-minutes.sql
@@ -0,0 +1,6 @@
+SELECT  DATEDIFF(MINUTE, SegmentStart, SegmentEnd)
+  FROM [dbo].[Streams]
+    WHERE SegmentStart > '2021-01-31 00:00' --reporting window
+  AND SegmentEnd < '2021-02-07 00:00' --reporting window
+  AND UserId IN (SELECT Id FROM Users)
+  GROUP BY SegmentId, SegmentStart, SegmentEnd
