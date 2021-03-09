9e9132a41ece951a2056ac696b40a59b765dcbbc
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 16:23:11 +0000

    Update total-meetings-organised.sql

--- TWAPerformance/tac-sql/total-meetings-organised.sql
+++ TWAPerformance/tac-sql/total-meetings-organised.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*)
   FROM [dbo].[Calls]
-  WHERE StartDateTime > '2021-01-31 00:00' --report window
-  AND EndDateTime < '2021-02-07 00:00'  --report window
+  WHERE StartDateTime > '2021-03-02 00:00' --report window
+  AND EndDateTime < '2021-03-09 00:00'  --report window
   AND Type = 2
   AND OrganiserUserId IN (SELECT Id FROM Users)
