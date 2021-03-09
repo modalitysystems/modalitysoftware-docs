5dba046c89ae2f45355b347f663a5c4fecc772db
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 16:38:38 +0000

    Update total-meeting-joins.sql

--- TWAPerformance/tac-sql/total-meeting-joins.sql
+++ TWAPerformance/tac-sql/total-meeting-joins.sql
@@ -4,4 +4,4 @@
     WHERE s.StartDateTime > '2021-01-31 00:00' -- reprting window
   AND s.EndDateTime < '2021-02-07 00:00' -- reprting window
   AND c.Type = 2
-  AND s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users)
+  AND (s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users))
