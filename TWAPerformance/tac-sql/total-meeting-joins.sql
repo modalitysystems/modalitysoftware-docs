e8ae235e75038e872e0f7497b41792bd08493014
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 15:40:14 +0000

    Update total-meeting-joins.sql

--- TWAPerformance/tac-sql/total-meeting-joins.sql
+++ TWAPerformance/tac-sql/total-meeting-joins.sql
@@ -1,6 +1,7 @@
-SELECT COUNT(DISTINCT SegmentId)
-  FROM [dbo].[Streams]
-    WHERE SegmentStart > '2021-01-31 00:00' -- reprting window
-  AND SegmentEnd < '2021-02-07 00:00' -- reprting window
-  AND CallType = 2
-  AND UserId IN (SELECT Id FROM Users)
+  SELECT COUNT(DISTINCT SegmentId)
+  FROM [dbo].Calls c
+  JOIN [dbo].[Segments] s on s.CallId = c.CallId 
+    WHERE s.StartDateTime > '2021-01-31 00:00' -- reprting window
+  AND s.EndDateTime < '2021-02-07 00:00' -- reprting window
+  AND c.Type = 2
+  AND s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users)
