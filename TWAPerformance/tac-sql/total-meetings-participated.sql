6f3b8c9b4f97e649ce8c8dedec1d1fa5cb86bdcc
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 16:12:03 +0000

    Update total-meetings-participated.sql

--- TWAPerformance/tac-sql/total-meetings-participated.sql
+++ TWAPerformance/tac-sql/total-meetings-participated.sql
@@ -3,5 +3,5 @@
   JOIN [dbo].[Segments] s on s.CallId = c.CallId 
     WHERE s.StartDateTime > '2021-03-02 00:00' --reporting window
   AND s.EndDateTime < '2021-03-09 00:00' --reporting window
-  AND s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users)
+  AND (s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users))
   AND c.Type = 2
