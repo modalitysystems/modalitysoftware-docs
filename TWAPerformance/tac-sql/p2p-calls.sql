42ade84021bd6a32e5e891815d96cf53ab180a56
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 16:29:32 +0000

    Update p2p-calls.sql

--- TWAPerformance/tac-sql/p2p-calls.sql
+++ TWAPerformance/tac-sql/p2p-calls.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(DISTINCT c.CallId)
+SELECT COUNT(*)
   FROM [dbo].Calls c
     WHERE c.StartDateTime > '2021-03-02 00:00' --reporting window
   AND c.EndDateTime < '2021-03-09 00:00' --reporting window
