d10b7ea3dd13d83566d1266c145cebe58b7c3ad8
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-09 16:29:13 +0000

    Update p2p-calls.sql

--- TWAPerformance/tac-sql/p2p-calls.sql
+++ TWAPerformance/tac-sql/p2p-calls.sql
@@ -1,7 +1,5 @@
 SELECT COUNT(DISTINCT c.CallId)
   FROM [dbo].Calls c
-  JOIN [dbo].[Segments] s on s.CallId = c.CallId 
-    WHERE s.StartDateTime > '2021-03-02 00:00' --reporting window
-  AND s.EndDateTime < '2021-03-09 00:00' --reporting window
-  AND (s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users))
+    WHERE c.StartDateTime > '2021-03-02 00:00' --reporting window
+  AND c.EndDateTime < '2021-03-09 00:00' --reporting window
   AND c.Type = 1
