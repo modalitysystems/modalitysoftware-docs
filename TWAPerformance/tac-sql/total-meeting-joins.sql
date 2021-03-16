87b91c19404333eba0b88c6a5011d305e10dbfb4
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-16 11:19:24 +0000

    Update total-meeting-joins.sql

--- TWAPerformance/tac-sql/total-meeting-joins.sql
+++ TWAPerformance/tac-sql/total-meeting-joins.sql
@@ -1,7 +1,5 @@
-  SELECT COUNT(DISTINCT SegmentId)
-  FROM [dbo].Calls c
-  JOIN [dbo].[Segments] s on s.CallId = c.CallId 
-    WHERE s.StartDateTime > '2021-01-31 00:00' -- reprting window
-  AND s.EndDateTime < '2021-02-07 00:00' -- reprting window
-  AND c.Type = 2
-  AND (s.CalleeId IN (SELECT Id FROM Users) OR s.CallerId IN (SELECT Id FROM Users))
+SELECT SUM(TotalConferenceCount) as TotalConferenceCount
+  FROM [aggregates].[ConferenceMinsByUsers]
+  WHERE [Date] > '2021-03-02 00:00' --reporting window
+  AND [Date] < '2021-03-09 00:00' --reporting window
+  AND [_HashUserId] IN (SELECT [_HashUserId] FROM Users)
