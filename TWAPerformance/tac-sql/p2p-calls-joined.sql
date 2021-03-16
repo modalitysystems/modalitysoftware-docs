c53c2ce52ca8b664a3da44ec316c3f4e37d00b09
Liam Jones <48244744+liiamjones@users.noreply.github.com>
2021-03-16 11:20:09 +0000

    Update p2p-calls-joined.sql

--- TWAPerformance/tac-sql/p2p-calls-joined.sql
+++ TWAPerformance/tac-sql/p2p-calls-joined.sql
@@ -1,5 +1,5 @@
-SELECT COUNT(*)
-  FROM [dbo].[Calls]
-  WHERE StartDateTime > '2021-01-31 00:00' -- reporting window
-  AND EndDateTime < '2021-02-07 00:00' -- reporting window
-  AND Type = 1
+  SELECT SUM(TotalCallCount) as TotalCallCount
+  FROM [aggregates].[P2PMinsByUsers]
+  WHERE [Date] > '2021-03-02 00:00' --reporting window
+  AND [Date] < '2021-03-09 00:00' --reporting window
+  AND [_HashUserId] IN (SELECT [_HashUserId] FROM Users)
